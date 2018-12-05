4c5f8754111f13001a490080f4b9f950b9bda5c7
Fiodar <f.motin@sam-solutions.com>
2018-12-05 23:16:45 +0300

    Add Mvvm.Cross support and font for dish 1 screen

---
+++ Craft_dish/Views/Dish1View.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Views;
+using Android.Media;
+using static Android.Resource;
+using Android.Content;
+
+namespace Craft_dish
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/NoActionBar", MainLauncher = true)]
+    public class Dish1View : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_dish1);
+        }
+    }
+}
